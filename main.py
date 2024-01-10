1efdf2bedeedb82372882aaee4b55db5009bfed3
Jean-CharlesAlinquant <jcalinquant@hotmail.com>
2024-01-10 16:21:19 +0000

    Saving data to file

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_data():
+    formatted_data = f"{website_entry.get()} | {username_entry.get()} | {password_entry.get()}\n"
+    with open("data.txt", "a") as file:
+        file.write(formatted_data)
+    print(formatted_data)
+    reset_entries()
+
+def reset_entries():
+    website_entry.delete(0, END)
+    username_entry.delete(0, END)
+    username_entry.insert(0, "jcalinquant@hotmail.com")
+    password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Row 0
 canvas = Canvas(width=200, height=200)
@@ -19,12 +31,14 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 
 # Row 2
 username_label = Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
 username_entry = Entry(width=35)
 username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.insert(0, "jcalinquant@hotmail.com")
 
 # Row 3
 password_label = Label(text="Password:")
@@ -35,7 +49,7 @@ generate_pwd_btn = Button(text="Generate Password")
 generate_pwd_btn.grid(row=3, column=2)
 
 # Row 4
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=add_data)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
